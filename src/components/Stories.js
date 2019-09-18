297ae7261d68bf08077bf39b49976f910297a945
Mefit Hernández <mefitdev@Mefits-MacBook-Pro.local>
2019-09-18 16:08:26 -0500

    ADD- Redux Setup

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import { loadStories, clearStories } from '../reducers'
 
-const Stories = () => {
+const Stories = (props) => {
     return (
         <div>
-            Stories component
+            <button type="button" onClick={()=>props.loadStories()}>Load Top 3 Stories</button>
+            <button type="button" onClick={()=>props.clearStories()}>Clear</button>
+            <StoryList {...props} />
         </div>
     )
 }
 
-export default Stories
+const StoryList = (props) => {
+    const hasNoLength = props.items.length === 0;
+    
+    if(hasNoLength) return <p>No hay</p>;
+    return (
+        <div>
+            {props.items.map(item => <Story {...item} key={item.id} />)}
+        </div>
+    )
+}
+
+const Story = ({title}) => {
+    return <p>{title}</p>
+}
+
+const mapStateToProps = (state)=> state;
+const mapDispatch = dispatch => {
+    return {
+        loadStories: ()=>dispatch(loadStories()),
+        clearStories: ()=>dispatch(clearStories())
+    }
+}
+export default connect(mapStateToProps, mapDispatch)(Stories);
